bdeeaf30898971f61abe93de7657bb371dbe21e7
Pavel Zhachko <rammiel@yandex.ru>
2020-12-13 23:20:04 +0300

    feat: add extractEmails(), getRectangleString() functional

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -175,8 +175,8 @@ function convertToUpperCase(str) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';');
 }
 
 /**
@@ -202,8 +202,10 @@ function extractEmails(/* str */) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleString(width, height) {
+  return `┌${'─'.repeat(width - 2)}┐\n`
+  + `${`│${' '.repeat(width - 2)}│\n`.repeat(height - 2)}`
+  + `└${'─'.repeat(width - 2)}┘\n`;
 }
 
 
